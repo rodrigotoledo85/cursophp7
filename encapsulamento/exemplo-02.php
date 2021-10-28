67c5f616b2145efab30c404ec0706c9291c8c9fb
Rodrigo Toledo <rodrigomelotoledo@aol.com>
2021-10-28 16:35:09 -0300

    encapsulamento

--- encapsulamento/exemplo-02.php
+++ encapsulamento/exemplo-02.php
@@ -11,7 +11,15 @@ class Pessoa{
         echo $this->senha. "</br>";
     }
 }
-
-$obj = new Pessoa();
+class Programador extends Pessoa{
+    public function verDados()
+    {
+        echo get_class($this). "<br/>";
+        echo $this->nome. "</br>";
+        echo $this->idade. "</br>";
+        echo $this->senha. "</br>";
+    }
+}
+$obj = new Programador();
 //echo $obj->nome. "</br>";
 $obj->verDados();
