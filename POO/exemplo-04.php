6ec2a4f7c640652f551f1620947986d9f07f628c
Rodrigo Toledo <rodrigomelotoledo@aol.com>
2021-10-28 15:06:00 -0300

    POO

---
+++ POO/exemplo-04.php
@@ -0,0 +1,13 @@
+<?php
+class Endereco{
+  private $logradouro;
+  private $numero;
+  private $cidade;
+
+  public function __construct($a, $b, $c){
+    $this->logradouro = $a;
+    $this->numero = $b;
+    $this->cidade = $c;
+  }
+}
+?>
