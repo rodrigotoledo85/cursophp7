fc59b05a98071fccc06c4e9c472396e1936b3602
Rodrigo Toledo <rodrigomelotoledo@aol.com>
2021-12-29 02:52:13 -0300

    POO

---
+++ abstrata/exemplo-01.php
@@ -0,0 +1,29 @@
+<?php
+interface Veiculo{
+  public function acelerar($velocidade);
+  public function frenar($velocidade);
+  public function trocarMarcha($marcha);
+}
+
+abstract class Automovel implements Veiculo{
+  public function acelerar($velocidade){
+    echo "O veiculo acelerou ate " . $velocidade . " Km/H";
+  }
+
+  public function frenar($velocidade){
+    echo "O veiculo frenou ate ". $velocidade ." Km/H";
+  }
+
+  public function trocarMarcha($marcha){
+    echo "O veiculo engatou a ". $marcha ." marcha";
+  }
+}
+
+class DelRey extends Automovel{
+  public function empurrar(){
+
+  }
+}
+$carro = new DelRey();
+$carro->acelerar(200);
+ ?>
