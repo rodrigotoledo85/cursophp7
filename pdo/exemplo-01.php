50724268887dcad035a0bece4d2adb357914ec7e
Rodrigo Toledo <rodrigomelotoledo@aol.com>
2021-12-29 23:49:09 -0300

    PDO

---
+++ pdo/exemplo-01.php
@@ -0,0 +1,19 @@
+<?php 
+
+$conn = new PDO("mysql:dbname=dbphp7; host=localhost", "root", "root");
+
+$stmt = $conn->prepare("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+$stmt->execute();
+
+$resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($resultado as $row) {
+	foreach ($row as $key => $value) {
+		echo "<strong>" . $key . ":</strong>" . $value . "<br>";
+	}
+	echo "========================================================<br>";
+}
+
+var_dump($resultado);
+ ?>
